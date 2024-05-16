c516020da2de97d0838cc30d58a78a0aa5d52b2e
Christian <spang.christian@yahoo.de>
2024-05-16 09:02:09 +0200

    add bookmark toggle feature

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// phew… not a lot going on here. Please add some code!
+console.clear();
+const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+
+function toggleBookmark() {
+  bookmarkButton.classList.toggle("bookmark--active");
+}
+
+bookmarkButton.addEventListener("click", toggleBookmark);
